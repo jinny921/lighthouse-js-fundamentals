0e13712e72796b226b94ad94855fd38ff1500ef4
Jinny Huang <jinny921@gmail.com>
2017-03-10 05:13:26 +0000

    loopy problem 100 to 200

---
+++ loopy-problem.js
@@ -0,0 +1,14 @@
+var numbers = [];
+for(var i = 100; i <= 200; i++) {
+    numbers.push(i);
+    // console.log(i);
+    if (i % 3 == 0 && i % 4 == 0) {
+        console.log("LoopyLighthouse");
+    } else if (i % 3 == 0) {
+        console.log("Loopy");
+    } else if (i % 4 == 0) {
+        console.log("Lighthouse");
+    } else {
+        console.log(i);
+    }
+}
